d400ed64623e09a40c802487cf7a4cc3d504895b
Addow Abdi <adowabdi007@icloud.com>
2025-02-03 07:36:33 -0600

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,29 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timeStamp}] ${method} to ${url}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'invalid user',
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user',
+    })
+  }
 }
 
 function validateUser(req, res, next) {
@@ -14,4 +34,10 @@ function validatePost(req, res, next) {
   // DO YOUR MAGIC
 }
 
-// do not forget to expose these functions to other modules
+// do not forget to expose these functions to other module
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware')
 const server = express();
+const UsersRouter = require('./users/users-router')
 
 server.use(express.json())
+
+server.use(logger)
+
 // global middlewares and the user's router need to be connected here
+
+server.use('/api/users', UsersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
